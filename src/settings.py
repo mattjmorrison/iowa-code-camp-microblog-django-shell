9b77193fbcfdd0633f7b50bdbd07eebd47edb03e
Matthew J Morrison <mattj.morrison@gmail.com>
2010-11-03 23:57:00 -0500

    added south to installed apps and changed database name to .database

--- src/settings.py
+++ src/settings.py
@@ -18,7 +18,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'data.base',                      # Or path to database file if using sqlite3.
+        'NAME': '.database',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -94,6 +94,7 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
+    'south',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
